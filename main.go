b189c69fe47923c46f0d8c2621f56bfa483162b3
Loric Brevet <loric.brevet@gmail.com>
2018-09-14 11:06:07 +0200

    Parse whole lines file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -36,29 +37,51 @@ const (
 )
 
 type Point struct {
+	X           float32
+	Y           float32
+	PenPressure float32
+	XRotation   float32
+	YRotation   float32
 }
 
 type Line struct {
 	BrushType  BrushType
 	BrushColor BrushColor
+	BrushSize  BrushSize
+	Points     []Point
 }
 
 type Layer struct {
+	Lines []Line
 }
 
 type Page struct {
-}
-
-type Notebook struct {
 	Layers []Layer
 }
 
-func NewLine(b BrushType, c BrushColor) Line {
-	return Line{b, c}
+type Notebook struct {
+	Name  string
+	Pages []Page
 }
 
-func NewNotebook() Notebook {
-	return Notebook{}
+func NewPoint(x, y, p, xr, yr float32) Point {
+	return Point{x, y, p, xr, yr}
+}
+
+func NewLine(t BrushType, c BrushColor, s BrushSize, p []Point) Line {
+	return Line{t, c, s, p}
+}
+
+func NewLayer(l []Line) Layer {
+	return Layer{l}
+}
+
+func NewPage(l []Layer) Page {
+	return Page{l}
+}
+
+func NewNotebook(n string, p []Page) Notebook {
+	return Notebook{n, p}
 }
 
 func ReadInt32(f *os.File) int {
@@ -67,7 +90,82 @@ func ReadInt32(f *os.File) int {
 	return int(binary.LittleEndian.Uint32(b))
 }
 
+func ReadFloat32(f *os.File) float32 {
+	b := make([]byte, 4)
+	f.Read(b)
+	bits := binary.LittleEndian.Uint32(b)
+	return math.Float32frombits(bits)
+}
+
 const Header = "reMarkable lines with selections and layers"
+
+func parse(fileName string) Notebook {
+	f, _ := os.Open(fileName)
+
+	// Check header
+	h := make([]byte, 43)
+	f.Read(h)
+	if string(h) != Header {
+		fmt.Println("Wrong input file type")
+		os.Exit(1)
+	}
+
+	// Get number of pages
+	nbPages := ReadInt32(f)
+
+	var pages []Page
+
+	for pageIdx := 1; pageIdx <= nbPages; pageIdx++ {
+
+		// Get number of layers
+		nbLayers := ReadInt32(f)
+
+		var layers []Layer
+
+		for layerIdx := 1; layerIdx <= nbLayers; layerIdx++ {
+			// Get number of lines
+			nbLines := ReadInt32(f)
+
+			var lines []Line
+
+			for lineIdx := 1; lineIdx <= nbLines; lineIdx++ {
+				brushType := ReadInt32(f)
+				brushColor := ReadInt32(f)
+				_ = ReadInt32(f) // Select and transform tool not used so far
+				brushSize := ReadFloat32(f)
+
+				// Get number of points
+				nbPoints := ReadInt32(f)
+
+				var points []Point
+
+				for pointIdx := 1; pointIdx <= nbPoints; pointIdx++ {
+					x := ReadFloat32(f)
+					y := ReadFloat32(f)
+					penPressure := ReadFloat32(f)
+					xRotation := ReadFloat32(f)
+					yRotation := ReadFloat32(f)
+
+					points = append(points, NewPoint(x, y, penPressure, xRotation, yRotation))
+				}
+
+				lines = append(lines, NewLine(
+					BrushType(brushType),
+					BrushColor(brushColor),
+					BrushSize(brushSize),
+					points,
+				))
+
+			}
+
+			layers = append(layers, NewLayer(lines))
+		}
+
+		pages = append(pages, NewPage(layers))
+	}
+
+	return NewNotebook(fileName, pages)
+}
 
 func main() {
 	// Get filename from flags
@@ -78,46 +176,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	f, _ := os.Open(*file)
-
-	// Check header
-	h := make([]byte, 43)
-	f.Read(h)
-	if string(h) != Header {
-		fmt.Println("Wrong input file type")
-		os.Exit(1)
-	}
-
-	// Initialize new Notebook
-	notebook := NewNotebook()
-
-	// Get number of pages
-	nbPages := ReadInt32(f)
-
-	for pageIdx := 1; pageIdx <= nbPages; pageIdx++ {
-		// Get number of layers
-		nbLayers := ReadInt32(f)
-
-		for layerIdx := 1; layerIdx <= nbLayers; layerIdx++ {
-			// Get number of lines
-			nbLines := ReadInt32(f)
-
-			for lineIdx := 1; lineIdx <= nbLines; lineIdx++ {
-				brushType := ReadInt32(f)
-				brushColor := ReadInt32(f)
-				// Select and transform tool not used so far
-				_ = ReadInt32(f)
-
-				line := NewLine(
-					BrushType(brushType),
-					BrushColor(brushColor),
-				)
-
-				fmt.Printf("%#v", line)
-			}
-
-		}
-	}
-
+	notebook := parse(*file)
 	fmt.Printf("%#v", notebook)
 }
